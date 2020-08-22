25e92e16a3740f52dd9bebaa73766d627825915b
buckleader <jasmin28virdi@gmail.com>
2020-08-23 03:21:18 +0530

    connectong two containers in docker

--- app.js
+++ app.js
@@ -1,5 +1,17 @@
 const express = require("express")
+const mysql = require('mysql');
 
+const con = mysql.createConnection({
+
+   host: "mysql",
+    
+    user: "root",
+    
+    password: "complexpassword",
+    
+    database: 'Customers'
+    
+    });
 const app = express();
 const port = process.env.PORT;
 
